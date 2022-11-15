5ba9dae317a96f59d8d958e5d511bd3c57629a5d
Nur A Alam <nuraalam376@gmail.com>
2022-11-15 15:27:39 +0600

    TypeScript Basic

---
+++ Basic/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 28;
+if (age < 50) {
+    age += 10;
+}
+console.log("Age is ", age);
+//# sourceMappingURL=index.js.map
---
+++ Basic/src/index.ts
@@ -0,0 +1,7 @@
+let age: number = 28;
+
+if(age < 50) {
+    age += 10;
+}
+
+console.log("Age is ", age);
